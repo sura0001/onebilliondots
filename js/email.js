beaf50046c19bcfc84dba7f90e0c711a7973eb70
sura0001 <kushwaha.surabhi03@gmail.com>
2024-06-26 23:27:39 +0530

    Add files via upload
    1billion Dots v1
    
    First stable official release of the website.

---
+++ js/email.js
@@ -0,0 +1,14 @@
+function sendMail(){
+
+let namee = document.getElementById("name");
+let email = document.getElementById("email");
+let phone = document.getElementById("phone");
+let msg = document.getElementById("msg");
+
+ 
+let link = 'mailto:'+ email.value + '?subject=' + encodeURIComponent("Contact from Website") + '&body=' + encodeURIComponent("Name: " + namee.value + "\nEmail: " + email.value + "\nPhone: " + phone.value + "\n\n" + msg.value);
+
+window.location.href = link;
+
+  
+}
